b4ae6bca264c27d9900de9d670e46be97f2a8158
ylnrahma <rahmaylyn20@gmail.com>
2023-05-03 11:11:58 +0700

    “Latihan Praktikum 9: Membuat relasi many to many”

--- resources/views/mahasiswas/index.blade.php
+++ resources/views/mahasiswas/index.blade.php
@@ -49,6 +49,7 @@
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger">Delete</button>
+                <a class="btn btn-warning" href="{{ route('nilai',$Mahasiswa->Nim) }}">Nilai</a>
             </form>
         </td>
     </tr>
